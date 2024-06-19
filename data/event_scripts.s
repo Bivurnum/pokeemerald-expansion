e3c82479c14b9989d9dfedf9d40053b28944ee3e
Bivurnum <147376167+Bivurnum@users.noreply.github.com>
2024-06-19 16:44:17 -0500

    Update event_scripts.s

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -55,6 +55,7 @@
 #include "constants/tv.h"
 #include "constants/union_room.h"
 #include "constants/vars.h"
+#include "constants/follow_me.h"
 #include "constants/weather.h"
 	.include "asm/macros.inc"
 	.include "asm/macros/event.inc"
