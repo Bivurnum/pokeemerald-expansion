7a9a896153743de07837f0078481cc9c7a24667c
Bivurnum <147376167+Bivurnum@users.noreply.github.com>
2025-04-23 20:20:19 -0500

    reimplemented FNPC_NPC_FOLLOWER_PARTY_PREVIEW

--- src/battle_main.c
+++ src/battle_main.c
@@ -1422,7 +1422,9 @@ static void CB2_PreInitIngamePlayerPartnerBattle(void)
         *savedCallback = gMain.savedCallback;
         *savedBattleTypeFlags = gBattleTypeFlags;
         gMain.savedCallback = CB2_PreInitIngamePlayerPartnerBattle;
-        ShowPartyMenuToShowcaseMultiBattleParty();
+        if (!PlayerHasFollowerNPC() || !FollowerNPCIsBattlePartner() || (FNPC_NPC_FOLLOWER_PARTY_PREVIEW && FollowerNPCIsBattlePartner()))
+            ShowPartyMenuToShowcaseMultiBattleParty();
+
         break;
     case 1:
         if (!gPaletteFade.active)
