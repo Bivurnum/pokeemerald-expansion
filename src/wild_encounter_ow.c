e44bee8a97077f22c37fe367efc16bd4790a475f
Bivurnum <147376167+Bivurnum@users.noreply.github.com>
2026-04-14 11:41:42 -0500

    Add comment for mid step

--- src/wild_encounter_ow.c
+++ src/wild_encounter_ow.c
@@ -211,6 +211,7 @@ void UpdateOverworldWildEncounter(void)
     
     DespawnAllOverworldWildEncounters(OWE_GENERATED, WILD_CHECK_REPEL);
     struct ObjectEvent* player = &gObjectEvents[gPlayerAvatar.objectEventId];
+    // Don't spawn if player is mid step.
     if (player->currentCoords.x != player->previousCoords.x || player->currentCoords.y != player->previousCoords.y)
         return;
 
