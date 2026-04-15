a6d0e4ccb7f9ddf543be4a2448c9c1c49fd860d5
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2026-04-15 13:46:38 +0100

    MOVEMENT_TYPE_WANDER_AROUND_OWE remains restricted

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -11970,7 +11970,7 @@ bool8 MovementType_OverworldWildEncounter_WanderAround_Step4(struct ObjectEvent 
 
     SetObjectEventDirection(objectEvent, chosenDirection);
     sprite->sTypeFuncId = 5;
-    if (CheckRestrictedOWEMovement(objectEvent, chosenDirection))
+    if (CheckRestrictedOWEMovement(objectEvent, chosenDirection) && objectEvent->movementType != MOVEMENT_TYPE_WANDER_AROUND_OWE)
         sprite->sTypeFuncId = 1;
 
     return TRUE;
